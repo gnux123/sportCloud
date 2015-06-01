8a889adea418974c11f55ff4eb07ba472d415be7
vincent.sue <gnux123@gmail.com>
2015-06-01 17:17:39 +0800

    complete header modules.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'佛祖保佑 \'永無BUG!');
+$(function(){
+
+	$(".cates > li a").hover(function(){
+		var _index = $(this).parent("li").index();
+
+		$(this).parent("li")
+			   .addClass("active")
+			   .siblings("li.active")
+			   .removeClass("active");
+
+		if(_index == 6 || _index == 8) {
+			$(".subNavs").hide();
+		}else{
+			$(".subNavs").show();
+			$(".subNavs ul").eq(_index).show().siblings("ul").hide();
+		}
+
+	});
+});
