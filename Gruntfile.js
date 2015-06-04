20cb30f0f7e06b46201bc16d574ec8add7161c6c
vincent.sue <gnux123@gmail.com>
2015-06-04 17:52:46 +0800

    add gruntfile.js settings copy function ignore 'layout' folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -351,7 +351,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            '!**/layout/**' //ignore folder
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
